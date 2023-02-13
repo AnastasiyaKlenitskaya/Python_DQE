bbcb1c0280162bde4c51be2d85cb637c226398cc
AnastasiyaKlenitskaya <anastasiya_klenitskaya@epam.com>
2023-02-13 11:52:12 +0100

    Updating fist task

--- Python_basics/Task_1.py
+++ Python_basics/Task_1.py
@@ -12,17 +12,20 @@ for i in range(len(my_list) - 1):  # Starting a loop for bubble sort
             my_list[j], my_list[j + 1] = my_list[j + 1], my_list[
                 j]  # Replacing the positions of neighboring elements
 
-odd_sum = 0  # Initialization of the int variable to collect sum of odd elements in my_list
-even_sum = 0  # Initialization of the int variable to collect sum of even elements in my_list
-
+odd_summ = 0        # Initialization of the int variable to collect summ of odd elements in my_list
+odd_counter = 0     # Initialization of the int variable to collect amount of add elements in my list
+even_summ = 0       # Initialization of the int variable to collect summ of even elements in my_list
+even_counter = 0    # Initialization of the int variable to collect amount of even elements in my list
 for i in range(len(my_list)):  # Starting a loop that will go threw all the elements in my_list
-    if i % 2 == 0:  # Check if selected element is odd
-        odd_sum += my_list[i]  # Summing up value of the element to odd_sum variable
+    if my_list[i] % 2 == 0:  # Check if selected element is odd
+        odd_summ += my_list[i]  # Summing up value of the element to odd_summ variable
+        odd_counter += 1        # increasing counter for odd elements
     else:  # Otherwise ( if selected element is even)
-        even_sum += my_list[i]  # Summing up value of the element to even_sum variable
+        even_summ += my_list[i]  # Summing up value of the element to even_summ variable
+        even_counter += 1       # increasing counter for even elements
 
-odd_average = odd_sum / (len(my_list) / 2)  # Calculating average value of odd elements
-even_average = even_sum / (len(my_list) / 2)  # Calculating average value of even elements
+odd_average = odd_summ / odd_counter  # Calculating average value of odd elements
+even_average = even_summ / even_counter  # Calculating average value of even elements
 
 # Printing results
 print("Odd numbers average : " + str(odd_average))
